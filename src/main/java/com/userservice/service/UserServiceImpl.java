e8780ab5302ab8a3fca1a1a07b9c0dd3ffb2fa9b
salgu1998 <salgu1998@gmail.com>
2022-06-25 18:54:32 +0900

    feature : user 정보 요청시 order m.s RestTemplate로 통신하여 정보 가져옴

--- src/main/java/com/userservice/service/UserServiceImpl.java
+++ src/main/java/com/userservice/service/UserServiceImpl.java
@@ -8,11 +8,17 @@ import com.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +33,8 @@ public class UserServiceImpl implements UserService{
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
     private final BCryptPasswordEncoder bcrypt;
+    private final RestTemplate restTemplate;
+    private final Environment env;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -54,8 +62,14 @@ public class UserServiceImpl implements UserService{
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
         final UserDto userDto = modelMapper.map(userEntity, UserDto.class);
 
-        List<OrderRes> orderList = new ArrayList<>();
-        userDto.setOrders(orderList);
+        final ResponseEntity<List<OrderRes>> orderResList = restTemplate.exchange(
+                String.format(env.getProperty("orderService.uri"), userId),
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<OrderRes>>() {}
+        );
+
+        userDto.setOrders(orderResList.getBody());
         return userDto;
     }
 
